2f55e8659fa80ec93d00db816e9196d53dc98574
DiscWord <79574841+DiscWord@users.noreply.github.com>
2021-04-16 12:59:05 +0300

    First Enemy
    Добавил бандита и начал писать скрипт патрулирования для него

---
+++ Battle for Vnvnc/Assets/Script/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float WalkDistance  = 6f;
+    [SerializeField] private float WalkSpeed = 1f;
+    [SerializeField] private float TimeWait = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 leftBounderyPosition;
+    private Vector2 rightBounderyPosition;
+
+    private void Start() 
+    {
+        rb = GetComponent<Rigidbody2D>();
+        leftBounderyPosition = transform.position;
+        rightBounderyPosition = leftBounderyPosition + Vector2.right * WalkDistance;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition((Vector2)transform.position + Vector2.right * WalkSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(leftBounderyPosition,rightBounderyPosition);
+    }
+
+}
